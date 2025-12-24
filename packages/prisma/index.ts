c7c2776b144da7dd8502dadd97f046ad7a3a07ff
wangdengfeng <1953819535@qq.com>
2025-12-24 09:47:27 +0800

    修改配置

--- packages/prisma/index.ts
+++
@@ -1,4 +0,0 @@
-// packages/prisma/index.ts
-export * from './generated/client/client'; // 导出 PrismaClient
-export * from './generated/client/models'; // 导出 User, Post 等所有模型
-export * from './generated/client/enums';  // 导出 Role 等所有枚举
