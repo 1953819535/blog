f108f3790f3e50bf336a455604795cc19709cbe2
wangdengfeng <1953819535@qq.com>
2025-12-26 11:46:51 +0800

    数据库连接配置调整

--- apps/api/src/modules/prisma/prisma.service.ts
+++ apps/api/src/modules/prisma/prisma.service.ts
@@ -1,13 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@my/prisma';
 import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
-export class PrismaService extends PrismaClient {
-  constructor() {
-    const adapter = new PrismaPg({
-      connectionString: process.env.DATABASE_URL as string,
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor(configService: ConfigService) {
+    const databaseURL = configService.get<string>('DATABASE_URL');
+    if (!databaseURL) {
+      throw new Error('DATABASE_URL is not defined in environment variables');
+    }
+
+    super({
+      adapter: new PrismaPg({
+        connectionString: databaseURL,
+      }),
     });
-    super({ adapter });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+    console.log('✅ Database connected successfully');
   }
 }
